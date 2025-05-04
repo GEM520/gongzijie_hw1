3bea9b591e2ea3c9ba4cff59550e552f243e0a55
GEM <1535461763@qq.com>
2025-05-04 23:05:12 +0800

    作业

---
+++ build/CMakeFiles/algebra.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algebra.dir/src/algebra.c.obj"
+  "CMakeFiles/algebra.dir/src/algebra.c.obj.d"
+  "CMakeFiles/algebra.dir/src/main.c.obj"
+  "CMakeFiles/algebra.dir/src/main.c.obj.d"
+  "algebra.exe"
+  "algebra.exe.manifest"
+  "algebra.pdb"
+  "libalgebra.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/algebra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/algebra.dir/src/main.c.obj.d
@@ -0,0 +1,16 @@
+CMakeFiles/algebra.dir/src/main.c.obj: \
+ D:\vscode\algebra\gongzijie_hw1\src\main.c \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/stdio.h \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/crtdefs.h \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw.h \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/vadefs.h \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/swprintf.inl \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ E:/Dev-Cpp\ 5.11\ TDM-GCC\ 4.9.2\ Setup/Dev-Cpp/MinGW64/x86_64-w64-mingw32/include/_mingw_print_pop.h \
+ D:/vscode/algebra/gongzijie_hw1/inc/algebra.h
